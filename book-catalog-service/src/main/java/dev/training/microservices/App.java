402078ff890fc1b1ae72937ceef297f385344841
Mohamed Nofal <nofal.a.mohamed@gmail.com>
2024-05-06 20:52:06 +0300

    Initial commit

---
+++ book-catalog-service/src/main/java/dev/training/microservices/App.java
@@ -0,0 +1,25 @@
+package dev.training.microservices;
+
+import dev.training.microservices.repository.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import dev.training.microservices.model.Book;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(BookRepository bookRepository){
+        return (args) -> {
+            bookRepository.save(new Book(null, "The Great Gatsby", "F. Scott Fitzgerald", 15.99, 10));
+            bookRepository.save(new Book(null, "To Kill a Mockingbird", "Harper Lee", 12.50, 2));
+            bookRepository.save(new Book(null, "1984", "George Orwell", 10.25, 5));
+            bookRepository.save(new Book(null, "Pride and Prejudice", "Jane Austen", 14.75, 9));
+        };
+    }
+}
---
+++ book-catalog-service/src/test/java/dev/training/microservices/BookAppTests.java
@@ -0,0 +1,132 @@
+package dev.training.microservices;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import dev.training.microservices.model.Book;
+import net.minidev.json.JSONArray;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BookAppTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void Should_Return_A_Book_When_Data_Is_Saved() {
+        ResponseEntity<String> response = restTemplate
+                .getForEntity("/books/1", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isEqualTo(1);
+
+        String title = documentContext.read("$.title");
+        assertThat(title).isEqualTo("The Great Gatsby");
+
+        String author = documentContext.read("$.author");
+        assertThat(author).isEqualTo("F. Scott Fitzgerald");
+
+        Double amount = documentContext.read("$.price");
+        assertThat(amount).isEqualTo(15.99);
+
+        Number quantity = documentContext.read("$.quantity");
+        assertThat(quantity).isEqualTo(10);
+    }
+
+    @Test
+    @DirtiesContext
+    void Should_Create_A_New_Book() {
+        Book newBook = new Book(null, "To Kill a Mockingbird", "Harper Lee", 12.50, 5);
+        ResponseEntity<Void> createResponse = restTemplate
+                .postForEntity("/books", newBook, Void.class);
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        URI locationOfBook = createResponse.getHeaders().getLocation();
+        ResponseEntity<String> getResponse = restTemplate
+                .getForEntity(locationOfBook, String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isNotNull();
+
+        String title = documentContext.read("$.title");
+        assertThat(title).isEqualTo("To Kill a Mockingbird");
+
+        String author = documentContext.read("$.author");
+        assertThat(author).isEqualTo("Harper Lee");
+
+        Double amount = documentContext.read("$.price");
+        assertThat(amount).isEqualTo(12.50);
+
+        Number quantity = documentContext.read("$.quantity");
+        assertThat(quantity).isEqualTo(5);
+    }
+
+    @Test
+    void Should_Return_A_Page_Of_Books() {
+        ResponseEntity<String> response = restTemplate
+                .getForEntity("/books?page=0&size=3", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        JSONArray page = documentContext.read("$[*]");
+        assertThat(page.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DirtiesContext
+    void Should_Update_A_Book() {
+        Book updatedBookInfo = new Book(null, null, "Nofal", null, 5);
+        HttpEntity<Book> request = new HttpEntity<>(updatedBookInfo);
+        ResponseEntity<Void> response = restTemplate
+                .exchange("/books/1", HttpMethod.PUT, request, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+        ResponseEntity<String> getResponse = restTemplate
+                .getForEntity("/books/1", String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isEqualTo(1);
+
+        String title = documentContext.read("$.title");
+        assertThat(title).isEqualTo("The Great Gatsby");
+
+        String author = documentContext.read("$.author");
+        assertThat(author).isEqualTo("Nofal");
+
+        Double amount = documentContext.read("$.price");
+        assertThat(amount).isEqualTo(15.99);
+
+        Number quantity = documentContext.read("$.quantity");
+        assertThat(quantity).isEqualTo(5);
+    }
+
+    @Test
+    @DirtiesContext
+    void Should_Delete_A_Book() {
+        ResponseEntity<Void> response = restTemplate
+                .exchange("/books/1",
+                        HttpMethod.DELETE, null, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+        ResponseEntity<String> getResponse = restTemplate
+                .getForEntity("/books/1", String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+}
